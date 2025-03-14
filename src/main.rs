cf3d06aed882d4d21b2efc69874b5febc8d01838
fpena2 <f.j.pena@wustl.edu>
2025-03-14 02:57:53 -0500

    initial impl

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,48 @@ use image::{Rgb, RgbImage};
 use rand::{Rng, SeedableRng};
 
 // NOTE: Add a 20px padding to the final image
-const IMG_HEIGHT: u32 = 400;
-const IMG_WIDTH: u32 = 400;
-const MASK_HEIGHT: usize = (IMG_HEIGHT / 80) as usize;
-const MASK_WIDTH: usize = (IMG_WIDTH / 80) as usize;
+const IMG_HEIGHT: usize = 400;
+const IMG_WIDTH: usize = 400;
+const MASK_HEIGHT: usize = IMG_HEIGHT / 80;
+const MASK_WIDTH: usize = IMG_WIDTH / 80;
+const LIGHT_BLUE: [u8; 3] = [131, 173, 208];
+const BACKGROUND_COLOR: [u8; 3] = [240, 240, 240];
 
 struct Icon(RgbImage);
 
 impl Icon {
-    fn new(seed: u64) -> Self {
-        let rgb_image = RgbImage::new(IMG_WIDTH, IMG_HEIGHT);
-        Icon(rgb_image)
+    fn new() -> Self {
+        let color = image::Rgb(BACKGROUND_COLOR);
+        Icon(RgbImage::from_pixel(IMG_WIDTH as u32, IMG_HEIGHT as u32, color))
+    }
+
+    fn func(&mut self, seed: u64){
+        let mask = gen_mask(seed); 
+        let mut mask_active: Vec<(std::ops::Range<usize>, std::ops::Range<usize>)> = vec![];
+        for (y, row) in mask.iter().enumerate(){
+            for (x, val) in row.iter().enumerate()
+            {
+                if *val == true {
+                    let x_start = x * 80;
+                    let x_end = x_start + 80;
+
+                    let y_start = y * 80;
+                    let y_end = y_start + 80;
+                    mask_active.push(((x_start..x_end), (y_start..y_end)));
+                }
+            }
+        }
+        
+        // dbg!(mask_active);
+        
+        for (x_range, y_range) in mask_active.iter() {
+            for x in x_range.clone() {
+                for y in y_range.clone(){
+                    let pixel = self.0.get_pixel_mut(x as u32, y as u32);
+                    *pixel = image::Rgb(LIGHT_BLUE);
+                }
+            }            
+        }
     }
 }
 
@@ -30,9 +61,10 @@ fn gen_mask(seed: u64) -> [[bool; MASK_WIDTH]; MASK_HEIGHT] {
     mask
 }
 
+
 fn main() {
-    println!("Hello, world!");
-    let icon = Icon::new(444);
+    let mut icon = Icon::new();
+    icon.func(4449);
     icon.0.save("test.png").unwrap();
 }
 
