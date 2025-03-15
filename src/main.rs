155a43566fd22e211bad206b3b920a9248ca7540
fpena2 <f.j.pena@wustl.edu>
2025-03-14 19:48:56 -0500

    Refactor code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use image::ImageBuffer;
-use image::{Rgb, RgbImage};
+use image::RgbImage;
 use rand::{Rng, SeedableRng};
+use std::ops::Range;
 
 const BLOCK_WIDTH: usize = 70;
 const BLOCK_HEIGHT: usize = 70;
@@ -21,58 +21,69 @@ const BACKGROUND_COLOR: [u8; 3] = [240, 240, 240];
 struct Icon(RgbImage);
 
 impl Icon {
-    fn new() -> Self {
-        let color = image::Rgb(BACKGROUND_COLOR);
-        Icon(RgbImage::from_pixel(IMG_WIDTH as u32, IMG_HEIGHT as u32, color))
+    fn new(seed: u64) -> Self {
+        let mut icon = Icon(RgbImage::from_pixel(
+            IMG_WIDTH as u32,
+            IMG_HEIGHT as u32,
+            image::Rgb(BACKGROUND_COLOR),
+        ));
+
+        let mask = Self::generate_random_mask(seed);
+        let active_blocks = Self::active_blocks(mask);
+        icon.draw(active_blocks);
+        icon
     }
 
-    fn func(&mut self, seed: u64){
-        let mask = gen_mask(seed); 
-        let mut mask_active: Vec<(std::ops::Range<usize>, std::ops::Range<usize>)> = vec![];
-        for (y, row) in mask.iter().enumerate(){
-            for (x, val) in row.iter().enumerate()
-            {
+    fn generate_random_mask(seed: u64) -> [[bool; MASK_WIDTH]; MASK_HEIGHT] {
+        let mut rng = rand::rngs::SmallRng::seed_from_u64(seed);
+        let mut mask = [[false; MASK_WIDTH]; MASK_HEIGHT];
+        for i in 0..5 {
+            // `c` and `e` are a reflection of `a` and `b`
+            let a = rng.random_bool(0.5);
+            let b = rng.random_bool(0.5);
+            let c = rng.random_bool(0.5);
+            mask[i] = [a, b, c, b, a];
+        }
+        mask
+    }
+
+    fn active_blocks(mask: [[bool; MASK_WIDTH]; MASK_HEIGHT]) -> Vec<(Range<usize>, Range<usize>)> {
+        let mut active: Vec<(Range<usize>, Range<usize>)> = vec![];
+        for (y, row) in mask.iter().enumerate() {
+            for (x, val) in row.iter().enumerate() {
                 if *val == true {
                     let x_start = x * BLOCK_WIDTH;
                     let x_end = x_start + BLOCK_WIDTH;
                     let y_start = y * BLOCK_HEIGHT;
                     let y_end = y_start + BLOCK_HEIGHT;
-                    mask_active.push(((x_start..x_end), (y_start..y_end)));
+                    active.push(((x_start..x_end), (y_start..y_end)));
                 }
             }
         }
-        
-        // dbg!(mask_active);
-        
-        for (x_range, y_range) in mask_active.iter() {
+        active
+    }
+
+    fn draw(&mut self, active_blocks: Vec<(Range<usize>, Range<usize>)>) {
+        for (x_range, y_range) in active_blocks.iter() {
             for x in x_range.clone() {
-                for y in y_range.clone(){
-                    let pixel = self.0.get_pixel_mut((x + PADDING) as u32, (y + PADDING) as u32);
+                for y in y_range.clone() {
+                    let pixel = self
+                        .0
+                        .get_pixel_mut((x + PADDING) as u32, (y + PADDING) as u32);
                     *pixel = image::Rgb(LIGHT_BLUE);
                 }
-            }            
+            }
         }
     }
-}
 
-fn gen_mask(seed: u64) -> [[bool; MASK_WIDTH]; MASK_HEIGHT] {
-    let mut rng = rand::rngs::SmallRng::seed_from_u64(seed);
-    let mut mask = [[false; MASK_WIDTH]; MASK_HEIGHT];
-    for i in 0..5 {
-        // `c` and `e` are a reflection of `a` and `b`
-        let a = rng.random_bool(0.5);
-        let b = rng.random_bool(0.5);
-        let c = rng.random_bool(0.5);
-        mask[i] = [a, b, c, b, a];
+    fn save(self, path: &str) -> Result<(), image::ImageError> {
+        self.0.save(path)
     }
-    mask
 }
-
 
 fn main() {
-    let mut icon = Icon::new();
-    icon.func(4449);
-    icon.0.save("test.png").unwrap();
+    let icon = Icon::new(1253);
+    icon.save("test.png").unwrap();
 }
 
 #[cfg(test)]
