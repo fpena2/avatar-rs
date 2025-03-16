5df338daf49c827c03e4b7706e1cb8d72573f3ca
fpena2 <f.j.pena@wustl.edu>
2025-03-16 18:04:26 -0500

    new impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,92 +1,76 @@
 use image::{Rgb, RgbImage};
 use rand::{Rng, SeedableRng};
 use rayon::prelude::*;
-use std::ops::Range;
 
-const BLOCK_WIDTH: usize = 70;
-const BLOCK_HEIGHT: usize = 70;
-const PADDING: usize = 35;
-
+// Blocks
+const BLOCK_WIDTH: u32 = 70;
+const BLOCK_HEIGHT: u32 = 70;
+const PADDING: u32 = 35;
+// Image
+const IMG_HEIGHT: u32 = PADDING + (BLOCK_HEIGHT * 5) + PADDING;
+const IMG_WIDTH: u32 = PADDING + (BLOCK_WIDTH * 5) + PADDING;
 // Colors
+type Color = Rgb<u8>;
 const LIGHT_BLUE_COLOR: Rgb<u8> = Rgb([131, 173, 208]);
 const BACKGROUND_COLOR: Rgb<u8> = Rgb([240, 240, 240]);
-
-struct Mask {
-    matrix: [[bool; Self::MASK_WIDTH]; Self::MASK_HEIGHT],
-}
-
-impl Mask {
-    const MASK_HEIGHT: usize = 5;
-    const MASK_WIDTH: usize = 5;
-
-    fn new(seed: u64) -> Self {
-        Mask {
-            matrix: Self::generate_matrix(seed),
-        }
-    }
-
-    fn generate_matrix(seed: u64) -> [[bool; Self::MASK_WIDTH]; Self::MASK_HEIGHT] {
-        let mut rng = rand::rngs::SmallRng::seed_from_u64(seed);
-        let mut mask = [[false; Self::MASK_WIDTH]; Self::MASK_HEIGHT];
-
-        for i in 0..Self::MASK_HEIGHT {
-            // `c` and `e` are a reflection of `a` and `b`
-            let a = rng.random_bool(0.5);
-            let b = rng.random_bool(0.5);
-            let c = rng.random_bool(0.5);
-            mask[i] = [a, b, c, b, a];
-        }
-        mask
-    }
-
-    fn expanded(&self, new_width: usize, new_height: usize) -> Vec<(Range<usize>, Range<usize>)> {
-        self.matrix
-            .par_iter()
-            .enumerate()
-            .flat_map(|(y, row)| {
-                row.par_iter()
-                    .enumerate()
-                    .filter(|&(_, &val)| val)
-                    .map(move |(x, _)| {
-                        let x_start = x * new_width;
-                        let x_end = x_start + new_width;
-                        let y_start = y * new_height;
-                        let y_end = y_start + new_height;
-                        (x_start..x_end, y_start..y_end)
-                    })
-            })
-            .collect()
-    }
-}
 
 pub struct Icon(RgbImage);
 
 impl Icon {
-    // An Icon will have 5 blocks
-    const IMG_HEIGHT: usize = PADDING + (5 * BLOCK_HEIGHT) + PADDING;
-    const IMG_WIDTH: usize = PADDING + (5 * BLOCK_WIDTH) + PADDING;
-
     pub fn new(seed: u64) -> Self {
-        let mut icon = Icon(RgbImage::from_pixel(
-            Self::IMG_WIDTH as u32,
-            Self::IMG_HEIGHT as u32,
-            BACKGROUND_COLOR,
-        ));
-
-        let mask = Mask::new(seed);
-        let ranges = mask.expanded(BLOCK_WIDTH, BLOCK_HEIGHT);
-        icon.draw(ranges);
-        icon
+        let mut canvas = RgbImage::from_pixel(IMG_WIDTH, IMG_HEIGHT, BACKGROUND_COLOR);
+        Self::draw(seed, &mut canvas);
+        Icon(canvas)
     }
 
-    fn draw(&mut self, active_ranges: Vec<(Range<usize>, Range<usize>)>) {
-        for (x_range, y_range) in active_ranges {
-            for y in y_range.start..y_range.end {
-                for x in x_range.start..x_range.end {
-                    let pixel = self
-                        .0
-                        .get_pixel_mut((x + PADDING) as u32, (y + PADDING) as u32);
-                    *pixel = LIGHT_BLUE_COLOR;
+    fn draw(seed: u64, canvas: &mut RgbImage) {
+        fn get_color(of_image: bool) -> Option<Color> {
+            if of_image {
+                Some(LIGHT_BLUE_COLOR)
+            } else {
+                None
+            }
+        }
+
+        let mut rng = rand::rngs::SmallRng::seed_from_u64(seed);
+        for block_index in (0..5).map(|v| v * 70) {
+            let base = PADDING + block_index;
+            let block_a_type = get_color(rng.random_bool(0.5));
+            let block_b_type = get_color(rng.random_bool(0.5));
+            let block_c_type = get_color(rng.random_bool(0.5));
+
+            // A
+            if let Some(pixel) = block_a_type {
+                for y in base..base + 70 {
+                    for x in PADDING + 0..PADDING + 70 {
+                        canvas.put_pixel(x, y, pixel);
+                    }
+                    // A's reflection
+                    for x in PADDING + 280..PADDING + 350 {
+                        canvas.put_pixel(x, y, pixel);
+                    }
+                }
+            }
+
+            // B
+            if let Some(pixel) = block_b_type {
+                for y in base..base + 70 {
+                    for x in PADDING + 70..PADDING + 140 {
+                        canvas.put_pixel(x, y, pixel);
+                    }
+                    // B's reflection
+                    for x in PADDING + 210..PADDING + 280 {
+                        canvas.put_pixel(x, y, pixel);
+                    }
+                }
+            }
+
+            // C
+            if let Some(pixel) = block_c_type {
+                for y in base..base + 70 {
+                    for x in PADDING + 140..PADDING + 210 {
+                        canvas.put_pixel(x, y, pixel);
+                    }
                 }
             }
         }
