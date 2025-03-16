12df98ec3ba5f13d12360696e82bda4c9dd6036a
fpena2 <f.j.pena@wustl.edu>
2025-03-16 18:04:26 -0500

    new impl

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use avatar_rs::Icon;
+use std::time::Instant;
 
 fn main() {
+    let start = Instant::now();
     let icon = Icon::new(1253);
     icon.save("test.png").unwrap();
+    let duration = start.elapsed();
+    println!("Time taken: {:?}", duration);
 }
