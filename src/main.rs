0d677220d6fe9bca76a2a6702d5b7745f72aeb8a
fpena2 <f.j.pena@wustl.edu>
2025-03-16 20:28:41 -0500

    Add benchmarks

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 use avatar_rs::Icon;
-use std::time::Instant;
 
 fn main() {
-    let start = Instant::now();
     let icon = Icon::new(1253);
     icon.save("test.png").unwrap();
-    let duration = start.elapsed();
-    println!("Time taken: {:?}", duration);
 }
